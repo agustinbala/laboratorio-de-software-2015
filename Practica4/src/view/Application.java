a2c2c2146bf423d73cfd5f7097caf9fd5fa4ff0d
natalia.bucar <natalia.bucar@globallogic.com>
2015-10-06 21:10:56 -0300

    se agrego la reproduccion musical

--- Practica4/src/view/Application.java
+++ Practica4/src/view/Application.java
@@ -1,44 +1,51 @@
 package view;
 
-import java.awt.Color;
+import java.awt.Button;
 import java.awt.Container;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import org.jfugue.player.Player;
+
 import view.domain.Figura;
 import view.domain.Nota;
 
 public class Application {
-	
+
 	static Figura figura;
 	static Nota nota;
 	static Label labelNotas;
+	static Button play;
+	static ArrayList<Nota> notas = new ArrayList<Nota>();
+	static ArrayList<Figura> figuras = new ArrayList<Figura>();
 
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException {
 		JFrame frame = new JFrame("Contenedor de Alto nivel");
 		frame.setSize(950, 450);
-		
-		
-		Container cont=frame.getContentPane();
-		JComponent contentpane=(JComponent) cont;
+
+		Container cont = frame.getContentPane();
+		JComponent contentpane = (JComponent) cont;
 		frame.getContentPane().setLayout(new GridLayout(1, 2));
-		
+
 		JPanel panelIzquierdo = new JPanel();
-		panelIzquierdo.setLayout(new BoxLayout(panelIzquierdo, BoxLayout.Y_AXIS));
-		
+		panelIzquierdo
+				.setLayout(new BoxLayout(panelIzquierdo, BoxLayout.Y_AXIS));
+
 		JPanel panel1 = new JPanel();
 		panel1.setSize(225, 750);
-		panel1.setLayout(new GridLayout(7, 1));		
+		panel1.setLayout(new GridLayout(7, 1));
 		panel1.add(createPanel(Figura.REDONDA));
 		panel1.add(createPanel(Figura.BLANCA));
 		panel1.add(createPanel(Figura.NEGRA));
@@ -47,7 +54,7 @@ public class Application {
 		panel1.add(createPanel(Figura.FUSA));
 		panel1.add(createPanel(Figura.SEMIFUSA));
 		panelIzquierdo.add(panel1);
-		
+
 		JPanel panel2 = new JPanel();
 		panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
 		panel2.setSize(225, 200);
@@ -55,78 +62,100 @@ public class Application {
 		labelNotas.setSize(100, 100);
 		panel2.add(labelNotas);
 		panelIzquierdo.add(panel2);
-		
+
+		JPanel panel3 = new JPanel();
+		panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
+		panel3.setSize(225, 200);
+		play = new Button("Play");
+		play.setSize(25, 25);
+		play.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				Player player = new Player();
+				labelNotas.setText("E/0.25 C/0.5 D/1 A/0.125");	
+				player.play(labelNotas.getText());
+
+			}
+		});
+
+		panel3.add(play);
+		panelIzquierdo.add(panel3);
 		contentpane.add(panelIzquierdo);
-		
-		
-		JPanelWithBackground panelImagen = new JPanelWithBackground("images/penta.png");
+
+		JPanelWithBackground panelImagen = new JPanelWithBackground(
+				"images/penta.png");
 		panelImagen.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				System.out.println("Coordenadas "+arg0.getX()+" "+arg0.getY());				
+				System.out.println("Coordenadas " + arg0.getX() + " "
+						+ arg0.getY());
 				setSelectedNote(arg0);
 			}
 
-			
 		});
-		contentpane.add(panelImagen);		
+		contentpane.add(panelImagen);
 
 		frame.show();
 
 	}
-	
+
 	private static void setSelectedNote(MouseEvent arg0) {
-		if(arg0.getX() >= 100 && arg0.getX() <= 140 && arg0.getY() <= 310 && arg0.getY() >= 280){
+		if (arg0.getX() >= 100 && arg0.getX() <= 140 && arg0.getY() <= 310
+				&& arg0.getY() >= 280) {
 			nota = Nota.C;
-		}else{
-			if(arg0.getX() >= 140 && arg0.getX() <= 180 && arg0.getY() <= 280 && arg0.getY() >= 250){
+		} else {
+			if (arg0.getX() >= 140 && arg0.getX() <= 180 && arg0.getY() <= 280
+					&& arg0.getY() >= 250) {
 				nota = Nota.D;
-			}
-			else{
-				if(arg0.getX() >= 180 && arg0.getX() <= 220 && arg0.getY() <= 250 && arg0.getY() >= 220){
+			} else {
+				if (arg0.getX() >= 180 && arg0.getX() <= 220
+						&& arg0.getY() <= 250 && arg0.getY() >= 220) {
 					nota = Nota.E;
-				}
-				else{
-					if(arg0.getX() >= 220 && arg0.getX() <= 260 && arg0.getY() <= 220 && arg0.getY() >= 190){
+				} else {
+					if (arg0.getX() >= 220 && arg0.getX() <= 260
+							&& arg0.getY() <= 220 && arg0.getY() >= 190) {
 						nota = Nota.F;
-					}
-					else{
-						if(arg0.getX() >= 260 && arg0.getX() <= 300  && arg0.getY() <= 190 && arg0.getY() >= 160){
+					} else {
+						if (arg0.getX() >= 260 && arg0.getX() <= 300
+								&& arg0.getY() <= 190 && arg0.getY() >= 160) {
 							nota = Nota.G;
-						}
-						else{
-							if(arg0.getX() >= 300 && arg0.getX() <= 340 && arg0.getY() <= 160 && arg0.getY() >= 130){
+						} else {
+							if (arg0.getX() >= 300 && arg0.getX() <= 340
+									&& arg0.getY() <= 160 && arg0.getY() >= 130) {
 								nota = Nota.A;
-							}else{
-								if(arg0.getX() >= 340 && arg0.getX() <= 380 && arg0.getY() <= 130 && arg0.getY() >= 100){
+							} else {
+								if (arg0.getX() >= 340 && arg0.getX() <= 380
+										&& arg0.getY() <= 130
+										&& arg0.getY() >= 100) {
 									nota = Nota.B;
-								}
-								else{
+								} else {
 									nota = null;
 								}
 							}
@@ -135,58 +164,63 @@ public class Application {
 				}
 			}
 		}
-		if(nota != null &&  figura != null){
-			System.out.println("La nota elegida es "+nota.getNota());	
-			labelNotas.setText(labelNotas.getText()+" "+nota.getNota()+figura.getDescripcion());
+		if (nota != null && figura != null) {
+			System.out.println("La nota elegida es " + nota.getNota());
+			labelNotas.setText(labelNotas.getText() + " " + nota.getNota()
+					+ figura.getDescripcion());			
 			nota = null;
 			figura = null;
 		} else {
-			System.out.println("No se eligió nunca nota");	
+			System.out.println("No se eligiï¿½ nunca nota");
+
 		}
-	}	
-	
-	private static JPanel createPanel(final Figura figuraTemp) throws IOException{
-		JPanel panel =  new JPanel();
+	}
+
+	private static JPanel createPanel(final Figura figuraTemp)
+			throws IOException {
+		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(1, 3));
-		panel.add(new Label(figuraTemp.getDescripcion()));
+		panel.add(new Label(figuraTemp.getNombre()));
 		panel.add(new Label("------------------------->"));
-		
-		JPanelWithBackground panelImagen = new JPanelWithBackground(figuraTemp.getImagen());
+
+		JPanelWithBackground panelImagen = new JPanelWithBackground(
+				figuraTemp.getImagen());
 		panelImagen.addMouseListener(new MouseListener() {
-			
+
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseEntered(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				
+
 			}
-			
+
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				System.out.println("La figura elegida es "+figuraTemp.getDescripcion());
-				Application.figura =  figuraTemp;
+				System.out.println("La figura elegida es "
+						+ figuraTemp.getDescripcion());
+				Application.figura = figuraTemp;
 			}
 		});
 		panel.add(panelImagen);
-		
+
 		return panel;
 	}
-	
+
 }
--- Practica4/src/view/domain/Figura.java
+++ Practica4/src/view/domain/Figura.java
@@ -2,21 +2,23 @@ package view.domain;
 
 public enum Figura {
 
-	REDONDA("Redonda", "images/redonda.png"),
-	BLANCA("Blanca", "images/blanca.png"),
-	NEGRA("Negra", "images/negra.png"),
-	CORCHEA("Corchea", "images/corchea.png"),
-	SEMICORCHEA("Semicorchea", "images/semicorchea.png"),
-	FUSA("Fusa", "images/fusa.png"),
-	SEMIFUSA("Semifusa", "images/semifusa.png");
+	REDONDA("Redonda","/1", "images/redonda.png"),
+	BLANCA("Blanca","/0.5", "images/blanca.png"),
+	NEGRA("Negra","/0.25", "images/negra.png"),
+	CORCHEA("Corchea","/0.125", "images/corchea.png"),
+	SEMICORCHEA("Semicorchea","/0.0625", "images/semicorchea.png"),
+	FUSA("Fusa","/0.03125", "images/fusa.png"),
+	SEMIFUSA("Semifusa","/0.015625", "images/semifusa.png");
 	
 
 	private String descripcion;
 	private String imagen;
+	private String nombre;
 	
-	private Figura(String descripcion, String imagen){
+	private Figura(String nombre, String descripcion, String imagen){
 		this.descripcion = descripcion;
 		this.imagen = imagen;
+		this.nombre = nombre;
 	}
 
 	public String getDescripcion() {
@@ -25,6 +27,14 @@ public enum Figura {
 
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public String getImagen() {
