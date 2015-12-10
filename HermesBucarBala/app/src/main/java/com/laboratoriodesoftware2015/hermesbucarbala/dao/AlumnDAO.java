cf636b9fb7f7a20de77583e7960ccbd6f0812f9e
Agustin Bala <agustin.bala@globallogic.com>
2015-12-09 22:29:09 -0300

    agregada parte de DAO y BD

--- HermesBucarBala/app/src/main/java/com/laboratoriodesoftware2015/hermesbucarbala/dao/AlumnDAO.java
+++ HermesBucarBala/app/src/main/java/com/laboratoriodesoftware2015/hermesbucarbala/dao/AlumnDAO.java
@@ -1,25 +1,25 @@
 package com.laboratoriodesoftware2015.hermesbucarbala.dao;
 
-import com.laboratoriodesoftware2015.hermesbucarbala.domain.Alumno;
+import com.laboratoriodesoftware2015.hermesbucarbala.domain.Alumn;
 
 /**
  * Created by AGUSTIN.BALA on 09-12-15.
  */
-public class AlumnDAO extends BaseDAO<Alumno> {
+public class AlumnDAO extends BaseDAO<Alumn> {
 
 
     @Override
-    void save(Alumno object) {
+    void save(Alumn object) {
 
     }
 
     @Override
-    void update(Alumno object) {
+    void update(Alumn object) {
 
     }
 
     @Override
-    void delete(Alumno object) {
+    void delete(Alumn object) {
 
     }
 }
--- HermesBucarBala/app/src/main/java/com/laboratoriodesoftware2015/hermesbucarbala/db/HermesSQLiteOpenHelper.java
+++ HermesBucarBala/app/src/main/java/com/laboratoriodesoftware2015/hermesbucarbala/db/HermesSQLiteOpenHelper.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.laboratoriodesoftware2015.hermesbucarbala.domain.Alumno;
+import com.laboratoriodesoftware2015.hermesbucarbala.domain.Alumn;
 import com.laboratoriodesoftware2015.hermesbucarbala.domain.Configuration;
 import com.laboratoriodesoftware2015.hermesbucarbala.domain.Pictogram;
 import com.laboratoriodesoftware2015.hermesbucarbala.domain.Tab;
@@ -23,21 +23,21 @@ public class HermesSQLiteOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+ Alumno.TABLE_NAME+" ( "+ Alumno.COLUMN_ID+" INT  PRIMARY KEY NOT NULL, "+ Alumno.COLUMN_NAME+" TEXT NOT NULL, "+ Alumno.COLUMN_LASTNAME+" TEXT NOT NULL, "+ Alumno.COLUMN_GENDER+"  CHAR(1) NOT NULL, "+ Alumno.COLUMN_SIZE+" TEXT )");
+        db.execSQL("CREATE TABLE "+ Alumn.TABLE_NAME+" ( "+ Alumn.COLUMN_ID+" INT  PRIMARY KEY NOT NULL, "+ Alumn.COLUMN_NAME+" TEXT NOT NULL, "+ Alumn.COLUMN_LASTNAME+" TEXT NOT NULL, "+ Alumn.COLUMN_GENDER+"  CHAR(1) NOT NULL, "+ Alumn.COLUMN_SIZE+" TEXT )");
         db.execSQL("CREATE TABLE "+Tab.TABLE_NAME+" ( "+Tab.COLUMN_ID+" INT PRIMARY KEY NOT NULL, "+Tab.COLUMN_NAME+" TEXT NOT NULL )");
-        db.execSQL("CREATE TABLE "+ Alumno.ALUMN_TAB_TABLE_NAME+" ( "+ Alumno.ALUMN_TAB_COLUMN_ALUMN_ID+" INT NOT NULL, "+ Alumno.ALUMN_TAB_COLUMN_TAB_ID+" INT KEY NOT NULL )");
+        db.execSQL("CREATE TABLE "+ Alumn.ALUMN_TAB_TABLE_NAME+" ( "+ Alumn.ALUMN_TAB_COLUMN_ALUMN_ID+" INT NOT NULL, "+ Alumn.ALUMN_TAB_COLUMN_TAB_ID+" INT KEY NOT NULL )");
         db.execSQL("CREATE TABLE "+Pictogram.TABLE_NAME+" ( "+Pictogram.COLUMN_ID+" INT PRIMARY KEY NOT NULL, "+Pictogram.COLUMN_NAME+" TEXT NOT NULL, "+Pictogram.COLUMN_FOLDER+" TEXT NOT NULL )");
-        db.execSQL("CREATE TABLE "+ Alumno.ALUMN_PICTOGRAM_TABLE_NAME+" ( "+ Alumno.ALUMN_PICTOGRAM_COLUMN_ALUMN_ID+" INT NOT NULL, "+ Alumno.ALUMN_PICTOGRAM_COLUMN_PICTOGRAM_ID+" INT NOT NULL)");
+        db.execSQL("CREATE TABLE "+ Alumn.ALUMN_PICTOGRAM_TABLE_NAME+" ( "+ Alumn.ALUMN_PICTOGRAM_COLUMN_ALUMN_ID+" INT NOT NULL, "+ Alumn.ALUMN_PICTOGRAM_COLUMN_PICTOGRAM_ID+" INT NOT NULL)");
         db.execSQL("CREATE TABLE "+Configuration.TABLE_NAME+" ( "+Configuration.COLUMN_ID+" INT PRIMARY KEY NOT NULL, "+Configuration.COLUMN_SERVER+" TEXT NOT NULL, "+Configuration.COLUMN_PORT+" TEXT NOT NULL )");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+ Alumno.TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ Alumn.TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS "+ Tab.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS "+ Alumno.ALUMN_TAB_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ Alumn.ALUMN_TAB_TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS "+ Pictogram.TABLE_NAME);
-        db.execSQL("DROP TABLE IF EXISTS "+ Alumno.ALUMN_PICTOGRAM_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS "+ Alumn.ALUMN_PICTOGRAM_TABLE_NAME);
         db.execSQL("DROP TABLE IF EXISTS "+ Configuration.TABLE_NAME);
         onCreate(db);
     }
